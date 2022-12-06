507e300752dce56c8c45ca1ef3c9e29ca9145316
ellon <mordecaimwema11@gmail.com>
2022-12-06 13:04:27 +0300

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+  
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
